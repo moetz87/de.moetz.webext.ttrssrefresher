9dba799e0c54974de54e7db0b58a2fe503a85b7e
Marco Oetz <marco.oetz@n-design.de>
2018-02-16 18:50:10 +0100

    Webpack-Config changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
-var srcdir = `${__dirname}`
-var dstdir = `${__dirname}`
+/*******************************
+ * PREPARE CONFIG
+ *******************************/
 
 var config = {
     context: __dirname,
@@ -14,42 +15,43 @@ var config = {
         }]
     },
     resolve: {
-        extensions: ['.ts', '.js']
+        extensions: ['.ts', '.js'],
+        modules: ['node_modules', `${__dirname}/shared`]
     }
 };
 
-var settingsConfig = Object.assign({}, config, {
-    entry: `${srcdir}/settings/main.ts`,
-    output: {
-        path: `${dstdir}/settings/`,
-        filename: 'index.js'
-    },
-});
+class Setting {
+    constructor(entry, output_path, output_name) {
+        this.entry = entry;
+        this.output_path = output_path;
+        this.output_name = output_name;
+    };
+}
 
-var contentScriptConfig1 = Object.assign({}, config, {
-    entry: `${srcdir}/content-scripts/refresher.ts`,
-    output: {
-        path: `${dstdir}/content-scripts/`,
-        filename: 'refresher.js'
-    },
-});
-
-var backgroundScriptConfig1 = Object.assign({}, config, {
-    entry: `${srcdir}/background-scripts/enable-disable-handler.ts`,
-    output: {
-        path: `${dstdir}/background-scripts/`,
-        filename: 'enable-disable-handler.js'
-    },
-});
-
-var backgroundScriptConfig2 = Object.assign({}, config, {
-    entry: `${srcdir}/background-scripts/refresh-triggerer.ts`,
-    output: {
-        path: `${dstdir}/background-scripts/`,
-        filename: 'refresh-triggerer.js'
-    },
-});
-
-module.exports = [
-    settingsConfig, backgroundScriptConfig1, backgroundScriptConfig2, contentScriptConfig1
+var settings = [
+    new Setting('settings/main.ts', 'settings/', 'index.js'),
+    new Setting('content-scripts/refresher.ts', 'content-scripts/', 'refresher.js'),
+    new Setting('background-scripts/enable-disable-handler.ts', 'background-scripts/', 'enable-disable-handler.js'),
+    new Setting('background-scripts/refresh-triggerer.ts', 'background-scripts/', 'refresh-triggerer.js')
 ];
+
+
+
+
+
+/*******************************
+ * CREATE CONFIG
+ *******************************/
+
+function mapToConfigObject(setting) {
+    var object = {
+        entry: `${__dirname}/${setting.entry}`,
+        output: {
+            path: `${__dirname}/${setting.output_path}`,
+            filename: setting.output_name
+        }
+    };
+    return Object.assign({}, config, object);
+}
+
+module.exports = settings.map(c => mapToConfigObject(c));
